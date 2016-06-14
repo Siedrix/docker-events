34bae887186720a0d5f1ab98079a2f7d55de71c0
Daniel Zavala <siedrix@gmail.com>
2016-06-14 18:15:11 -0500

    Fix remove-all

--- remove-all.js
+++ remove-all.js
@@ -1,4 +1,4 @@
-// env DOCKER_TYPE=certs DOCKER_HOST="192.168.99.100" DOCKER_PORT=2376 node remove.js
+// env DOCKER_TYPE=certs DOCKER_HOST="192.168.99.100" DOCKER_PORT=2376 node remove-all.js
 const Docker = require('dockerode')
 
 const env = require('./env')
@@ -21,7 +21,7 @@ modem.listContainers({all: true}, function (err, containers) {
 
 			console.log('info:',data.Name, data.State.Status)
 
-			if(data.State.Status === 'stop'){
+			if(data.State.Status === 'stop' || data.State.Status === 'exited'){
 				console.log('removing', data.Name)
 				container.remove(function(err){
 					if(err){
